600567c7e88a840e1cc3cd4746fdda023442f436
Gaurang Jain <gjboxer19122001@gmail.com>
2023-05-24 15:42:29 +0530

    all

--- src/Components/LoginPage/LoginPage.tsx
+++ src/Components/LoginPage/LoginPage.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import googleImage from '../../assets/googleImage.png';
-import githubImage from '../../assets/githubImage.png';
-import twitterImage from '../../assets/twitterImage.png';
 import styles from './LoginPage.module.css';
 
 export default function LoginPage() {
@@ -10,32 +8,15 @@ export default function LoginPage() {
         window.open("http://localhost:4000/auth/google", "_self");
     }
 
-    const githubLogin = () => {
-        window.open("http://localhost:4000/auth/github", "_self");
-    }
-    
-    const twitterLogin = () => {
-        window.location.href = "http://localhost:4000/auth/twitter"
-    }
 
 
-    return (
+   return (
         <div className={styles.loginPage}>
             <div className={styles.loginForm}>
                 <h1>Login</h1>
                 <div className={styles.googleContainer} onClick={googleLogin}>
                     <img src={googleImage} alt="Google Icon" />
                     <p>Login With Google</p>
-                </div>
-
-                <div className={`${styles.googleContainer} ${styles.githubContainer}`} onClick={githubLogin}>
-                    <img src={githubImage} alt="Github Icon" />
-                    <p>Login With Github</p>
-                </div>
-
-                <div className={`${styles.googleContainer} ${styles.twitterContainer}`} onClick={twitterLogin}>
-                    <img src={twitterImage} alt="Twitter Icon" />
-                    <p>Login With Twitter</p>
                 </div>
 
             </div>
--- src/types/maintypes.ts
+++ src/types/maintypes.ts
@@ -1,7 +1,5 @@
 export interface IUser {
     googleId?: string;
-    twitterId?: string;
-    githubId?: string;
     username: string;
     __v: number;
     _id: string;
